1d0f0ea03a8f743109c513a4df100e9e7d091e02
Mohamed yassine ELKHADIRI <mohamedyassine.elkhadiri@insat.u-carthage.tn>
2022-10-13 22:46:04 +0100

    DONE

--- LSP/src/com/directi/training/lsp/exercise/Duck.java
+++ LSP/src/com/directi/training/lsp/exercise/Duck.java
@@ -1,12 +1,13 @@
 package com.directi.training.lsp.exercise;
 
-public class Duck
-{
+public class Duck implements IDuck
+{   
+    @Override
     public void quack()
     {
         System.out.println("Quack...");
     }
-
+    @Override
     public void swim()
     {
         System.out.println("Swim...");
